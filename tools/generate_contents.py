5fafa028fa94a84dd7365f53ad2ae296beaee674
Matt Craig <mattwcraig@gmail.com>
2018-07-04 22:08:22 -0500

    Add more features to TOC generator

--- tools/generate_contents.py
+++ tools/generate_contents.py
@@ -12,6 +12,8 @@ REG = re.compile(r'(\d\d)\.(\d\d)-(.*)\.ipynb')
 
 NO_NUMBER = ['00']
 
+TOC_STYLES = ['header_ulist', 'nested_list']
+
 
 def iter_notebooks(directory):
     return sorted(nb for nb in os.listdir(NOTEBOOK_DIR) if REG.match(nb))
@@ -32,29 +34,48 @@ def get_notebook_title(nb_file):
         raise ValueError('No title found for {}.'.format(nb_file))
 
 
-def gen_contents(directory=None, path_prefix=None):
+def gen_contents(directory=None, path_prefix=None,
+                 auto_number=False, toc_style=None):
+
+    current_chapter = -1
     for nb in iter_notebooks(directory):
         if path_prefix:
             nb_url = os.path.join(path_prefix, nb)
         else:
             nb_url = nb
         chapter, section, title = REG.match(nb).groups()
-        title = get_notebook_title(nb)
-        if section == '00':
-            if chapter in NO_NUMBER:
-                yield '\n### [{0}]({1})'.format(title, nb_url)
-            else:
-                yield '\n### [{0}. {1}]({2})'.format(int(chapter),
-                                                     title, nb_url)
+
+        # Generate auto chapter and section numbers even if
+        # we do not end up using them
+        if chapter != current_chapter:
+            current_chapter = chapter
+            current_section = 1
         else:
-            yield "- [{0}]({1})".format(title, nb_url)
+            current_section += 1
+
+        title = get_notebook_title(nb)
+
+        if section == '00':
+            if toc_style == 'nested_list':
+                yield f'\n{chapter}. [{title}]({nb_url})'
+            else:
+                if chapter in NO_NUMBER:
+                    yield '\n### [{0}]({1})'.format(title, nb_url)
+                else:
+                    yield '\n### [{0}. {1}]({2})'.format(int(chapter),
+                                                         title, nb_url)
+        else:
+            if toc_style == 'nested_list':
+                yield f'    {section}. [{title}]({nb_url})'
+            else:
+                yield "- [{0}]({1})".format(title, nb_url)
 
 
-def contents(directory=None, path_prefix=None):
+def contents(directory=None, **kwd):
     """
     Generate the table of contents as a string.
     """
-    return '\n'.join(gen_contents(directory))
+    return '\n'.join(gen_contents(directory, **kwd))
 
 
 def write_notebook(toc, filename, toc_name="Table of Contents"):
@@ -67,7 +88,10 @@ def write_notebook(toc, filename, toc_name="Table of Contents"):
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Generate a table of contents from a '
-                                        'directory of notebooks')
+                                        'directory of notebooks. The notebooks '
+                                        'should be named so that when sorted '
+                                        'by name they are in the order they '
+                                        'should appear in the table of contents.')
     parser.add_argument('-o', '--output',
                         help='Destination for the table of contents. If the '
                              'extension is .ipynb then the output is a '
@@ -81,9 +105,16 @@ if __name__ == '__main__':
                         help='If set, this string will be prepended to the '
                              'links to the notebook in the generated table '
                              'of contents.')
+    parser.add_argument('--auto-number', action='store_true',
+                        help='Automatically number table of contents entries.')
+    parser.add_argument('--toc-style', choices=TOC_STYLES,
+                        default='nested_list',
+                        help='Set the style for the table of contents.')
     args = parser.parse_args()
     directory = args.directory or NOTEBOOK_DIR
-    toc = contents(directory, path_prefix=args.path_prefix)
+    toc = contents(directory, path_prefix=args.path_prefix,
+                   auto_number=args.auto_number,
+                   toc_style=args.toc_style)
     if not args.output:
         print(toc)
     elif args.output.endswith('.ipynb'):
