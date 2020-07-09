758d3527781203015e384f89ba0ad983ef04cc5a
Matt Craig <mattwcraig@gmail.com>
2020-07-09 15:11:48 -0500

    Update navigation links to match new index location

--- tools/add_navigation.py
+++ tools/add_navigation.py
@@ -16,7 +16,7 @@ def prev_this_next(it):
 
 
 PREV_TEMPLATE = "< [{title}]({url}) "
-CONTENTS = "| [Contents](Index.ipynb) |"
+CONTENTS = "| [Contents](00.00-index.ipynb) |"
 NEXT_TEMPLATE = " [{title}]({url}) >"
 NAV_COMMENT = "<!--NAVIGATION-->\n"
 
